270613c0ecda650dcbbc790b1eb8dd8aece78122
Mangila <olsson.erik1993@gmail.com>
2022-01-06 16:17:45 +0100

    :hammer:

--- src/test/java/com/github/mangila/springrestfuljpa/persistence/repository/DirectorRepositoryTest.java
+++ src/test/java/com/github/mangila/springrestfuljpa/persistence/repository/DirectorRepositoryTest.java
@@ -3,13 +3,21 @@ package com.github.mangila.springrestfuljpa.persistence.repository;
 import com.github.mangila.springrestfuljpa.config.JpaConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.TestDatabaseAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@DataJpaTest(excludeAutoConfiguration = TestDatabaseAutoConfiguration.class)
 @Import(value = {JpaConfig.class})
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:moviedb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;MODE=MYSQL",
+        "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.dialect.storage_engine=innodb"
+})
 class DirectorRepositoryTest {
 
     @Autowired
--- src/test/java/com/github/mangila/springrestfuljpa/web/controller/MovieControllerTest.java
+++ src/test/java/com/github/mangila/springrestfuljpa/web/controller/MovieControllerTest.java
@@ -1,7 +1,43 @@
 package com.github.mangila.springrestfuljpa.web.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+@Slf4j
 class MovieControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Container
+    private final MySQLContainer<?> container = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+            .withLogConsumer(new Slf4jLogConsumer(log));
+
+    @Test
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/movie")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isEmpty());
+    }
 }
